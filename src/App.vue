4e95ef49a66b5fdce0873f9cdd58facb4c3d1991
joey <joey.huang@tech-core.org>
2017-11-14 19:11:12 +0800

    QQ分分彩

--- src/App.vue
+++ src/App.vue
@@ -176,8 +176,8 @@ export default {
                 // {class: 'ds-icon-game-bjssc', id: '1-1-4', menuid: '73', title: '北京时时彩', gameid: 17},
                 {class: 'ds-icon-game-hlffc', id: '1-1-4', menuid: '73', title: '欢乐分分彩', gameid: 17},
                 {class: 'ds-icon-game-twssc', id: '1-1-5', menuid: '76', title: '台湾5分彩', gameid: 20},
-                {class: 'ds-icon-game-ffctx sign hot', id: '1-2-6', menuid: '8', title: 'QQ分分彩', gameid: 2},
-                {class: 'ds-icon-game-ffcqq sign hot', id: '1-1-7', menuid: '96', title: '腾讯分分彩', gameid: 29},
+                {class: 'ds-icon-game-ffcqq sign hot', id: '1-2-6', menuid: '8', title: 'QQ分分彩', gameid: 2},
+                {class: 'ds-icon-game-ffctx sign hot', id: '1-1-7', menuid: '96', title: '腾讯分分彩', gameid: 29},
                 {url: 'HC6', class: 'ds-icon-game-lhc', id: '1-1-6', menuid: '95', title: '六合彩', gameid: 28}
               ]
             },
