599a75cd6212a3af0e28e34cdca633a489688126
joey <joey.huang@tech-core.org>
2017-11-27 14:26:51 +0800

    菜单对应

--- DSM/src/components/Menus.vue
+++ DSM/src/components/Menus.vue
@@ -1,5 +1,5 @@
 <template lang="jade">
-  el-menu(router=true @open="handleopen" @close="handleclose"  default-active="/me/2-1-1")
+  el-menu(router=true @open="handleopen" @close="handleclose"  default-active=" '/me/2-1-1' ")
     el-submenu(v-for="(m, i) in menus" v-if="m.title && menus.length > 2" v-bind:index="m.class")
       template(slot="title")
         i(:class="[m.class + '-menu']")
@@ -10,9 +10,9 @@
 
     div(v-for="(m, i) in menus"  v-if="menus.length === 1" v-bind:class=" [ menus[0].url ] ")
 
-      el-menu-item-group(v-for="(g, ii) in m.groups" v-if="g.title && g.items.filter(function(x){return !x.removed})[0]")
+      el-menu-item-group(v-for="(g, ii) in m.groups" v-if="g.items.filter(function(x){return !x.removed})[0]")
           template(slot="title") {{ g.title }}
-          el-menu-item.ds-button.full(v-for="(item, iii) in g.items" v-bind:index=" '/' + m.url + '/' + item.id" v-bind:class="[ item.class ]" v-if="!item.removed && item.id") {{ item.title }}
+          el-menu-item.ds-button.full(v-for="(item, iii) in g.items" v-bind:index=" '/' + m.url + '/' + item.id" v-bind:class="[ item.class, {'is-active': defaultUrl === ('/' + m.url + '/' + item.id), 'notis-active': defaultUrl !== ('/' + m.url + '/' + item.id)} ]" v-if="!item.removed && item.id") {{ item.title }}
 
 </template>
 
@@ -23,10 +23,14 @@ export default {
     handleopen () {
     },
     handleclose () {
-    }
+    },
+    defaultUrl: ''
   },
   computed: {
-    default () {
+  },
+  watch: {
+    '$route' ({path}) {
+      this.defaultUrl = path
     }
   }
 }
