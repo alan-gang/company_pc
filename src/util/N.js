27964562a8bb2039ec0396d2f1099d7f9aafffe7
joey <joey.huang@tech-core.org>
2018-01-04 17:12:16 +0800

    PK10　猜前三四名

--- src/util/N.js
+++ src/util/N.js
@@ -1378,6 +1378,11 @@ let PK10 = {
     // return [N(value, 5).length, N(value, 5)]
     return [N(value, 4, 1, 2, 10, 1).length, N(value, 4, 1, 2, 10, 1)]
   },
+  // title: '猜三四名单式'
+  '-1-1-9-PK10' ({value}) {
+    // return [N(value, 5).length, N(value, 5)]
+    return [N(value, 4, 1, 2, 10, 1).length, N(value, 4, 1, 2, 10, 1)]
+  },
   // title: '猜前三名
   '-1-1-3-PK10' ({ns}) {
     let n = 0
