15eb3b4e7855cba37c715c038630cea5a3724bc3
joey <joey.huang@tech-core.org>
2019-02-26 14:07:56 +0800

    pre

--- src/App.v2.vue
+++ src/App.v2.vue
@@ -277,7 +277,6 @@ export default {
                 {url: 'SSL3D', class: 'ds-icon-game-shssl', id: '1-5-4', menuid: '100', title: '上海时时乐', volume: true, gameid: 5},
                 {url: 'SSL', class: 'ds-icon-game-pl35', id: '1-5-3', menuid: '61', title: '排列三、五', volume: true, gameid: 10},
                 {url: 'HC6', class: 'ds-icon-game-lhc', id: '1-1-6', menuid: '95', title: '六合彩', volume: true, gameid: 28},
-                {url: 'PCDD', class: 'ds-icon-game-pcdd', id: '1-1-8', menuid: '112', title: 'PC蛋蛋', volume: false, gameid: 45},
                 // {menuid: '98', class: 'ds-icon-game-jn sign', id: '1-7-1', title: 'KG', fn: '18:203'},
                 {url: 'IFRAME', hide: true, class: 'ds-icon-game-lhc', id: '1-8-1', menuid: '98', title: '六合彩', volume: true, gameid: 28}
               ]
@@ -287,7 +286,8 @@ export default {
               url: 'KG',
               items: [
                 {menuid: '98', class: 'ds-icon-game-jn sign hot', id: '1-7-1', title: 'KG', fn: '18:203'},
-                {url: 'KL8', class: 'ds-icon-game-kl8 sign hot', id: '1-5-9', menuid: '92', title: '北京快乐8', volume: true, gameid: 27}
+                {url: 'KL8', class: 'ds-icon-game-kl8 sign hot', id: '1-5-9', menuid: '92', title: '北京快乐8', volume: true, gameid: 27},
+                {url: 'PCDD', class: 'ds-icon-game-pcdd', id: '1-1-8', menuid: '112', title: 'PC蛋蛋', volume: false, gameid: 45}
               ]
             }
             // {
