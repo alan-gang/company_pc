6aa611a30aebbf6e05de6ac5b54465efe0877242
brian <brian>
2019-05-15 15:26:58 +0800

    盈亏总表

--- src/views/myGameRecord/report.vue
+++ src/views/myGameRecord/report.vue
@@ -6,7 +6,7 @@
   slot(name="resize-y")
   slot(name="toolbar")
   .scroll-content
-    .search-bar.pl20
+    .search-bar.pl20(v-show=" [0].indexOf(I) == -1 ")
         SearchConditions(v-bind:showBtnSearch="true" @choiced="choicedSearchCondition" @search="search")
 
     template(v-if=" I === 0 ")
@@ -21,60 +21,17 @@
       el-table.header-bold.nopadding(:data="otherCommonReportData" style="margin: .2rem 0" stripe ref="table")  
         el-table-column(v-bind:prop="k" v-bind:label="v" v-for="(v, k, i) in otherCommonTableColumn" v-bind:class-name="i === 0 ? 'pl2' : ''")
     
-    //- el-table.header-bold.nopadding(:data="data" style="margin: .2rem 0" stripe ref="table")
-        el-table-column(prop="payerTime" label="充值时间" class-name="pl2")
-
-        el-table-column(prop="bankName" label="银行" )
-
-        el-table-column(prop="payerRealAmount" label="金额"  align="right")
-
-        el-table-column(prop="payerTransferFee" label="手续费"  align="right")
-
-        el-table-column(label="状态" align="center")
-          template(scope="scope")
-            span(:class="{ 'text-green': scope.row.isDone === '充值成功', 'text-danger': scope.row.isDone !== '充值成功' }") {{  scope.row.isDone}}
-
-        el-table-column(label="操作" align="center")
-          template(scope="scope")
-            span.ds-button.text-button(:class="{ blue: scope.row.errorEntry === '0', 'light wg': scope.row.errorEntry !== '0' }" v-if="scope.row.isDone !== '充值成功'" @click="showReq(scope.row)" style="padding: 0") 催到帐
-
     el-pagination(:total="totalSize" v-bind:page-size="pageSize" layout="prev, pager, next, total" v-bind:page-sizes="[5, 10, 15, 20]" v-bind:current-page="curPage" small v-if=" totalSize > 20 " v-on:current-change="pageChanged")
 
-
-  //- Summary.scroll-content(v-if=" I === 0 ")
-  //- Order.scroll-content(v-if=" I === 1 ")
-  //- VROrder.scroll-content(v-if=" I === 2 ")
-  //- SportRecord.scroll-content(v-if=" I === 3 ")
-  //- VideoRecord.scroll-content(v-if=" I === 4 ")
-  //- GameRecord.scroll-content(v-if=" I === 5 ")
-  //- FishRecord.scroll-content(v-if=" I === 6 ")
-  //- CardRecord.scroll-content(v-if=" I === 7 ")
 </template>
 
 <script>
-// import Summary from './Summary'
-// import Order from './OF'
-// import VROrder from './VROF'
-// import SportRecord from './SportRecord'
-// import VideoRecord from './VideoRecord'
-// import GameRecord from './GameRecord'
-// import FishRecord from './FishRecord'
-// import CardRecord from './CardRecord'
 import api from 'src/http/api'
 import { dateFormat } from '../../util/Date'
 import SearchConditions from 'components/SearchConditions'
 import store from '../../store'
 export default {
   components: {
-    // Summary,
-    // Order,
-    // VROrder,
-    // SportRecord,
-    // VideoRecord,
-    // GameRecord,
-    // FishRecord,
-    // CardRecord
-
     SearchConditions
   },
   name: 'my-game-record',
@@ -88,6 +45,7 @@ export default {
       curPage: 0,
       totalSize: 0,
       profitAndLossSummaryTableColumn: {
+        date: '时间',
         chesettle: '棋牌盈亏',
         egamesettle: '电游盈亏',
         esptsettle: '电竞盈亏',
@@ -170,7 +128,8 @@ export default {
       this.$http.get(api.personalReport).then(({data: {success, items, totalSize}}) => {
         if (success === 1) {
           if (items.length > 0) {
-            this.profitAndLossSummaryData = items.slice(items.length - 1)
+            items[items.length - 1].date = '赢亏汇总'
+            this.profitAndLossSummaryData = items // items.slice(items.length - 1)
           }
         }
       }).finally(() => {
