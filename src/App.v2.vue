83360e0cb3d408a91f22a11de1eaa0b82ef6cbf1
joey <joey.huang@tech-core.org>
2019-09-27 18:55:44 +0800

    奇趣腾讯...

--- src/App.v2.vue
+++ src/App.v2.vue
@@ -240,11 +240,11 @@ export default {
                 {class: 'ds-icon-game-ffc_aly sign new', id: '1-1-122', menuid: '122', title: '阿里云分分彩', volume: false, gameid: 161},
                 {class: 'ds-icon-game-hnwfc sign new', id: '1-1-119', menuid: '119', title: '河内5分彩', volume: true, gameid: 158},
                 {class: 'ds-icon-game-hnyfc sign new', id: '1-1-126', menuid: '126', title: '河内1分彩', volume: true, gameid: 163},
-                {class: 'ds-icon-game-ffc_qq sign hot', id: '1-1-127', menuid: '127', title: '奇趣分分彩', volume: false, gameid: 164},
+                {class: 'ds-icon-game-qqtxffc sign hot', id: '1-1-127', menuid: '127', title: '奇趣腾讯分分彩', volume: false, gameid: 164},
                 {class: 'ds-icon-game-tx2fcjs sign odd', id: '1-1-10', menuid: '113', title: '腾讯2分彩', subTitle: '奇数', volume: true, gameid: 152},
                 {class: 'ds-icon-game-tx2fcos sign even', id: '1-1-11', menuid: '114', title: '腾讯2分彩', subTitle: '偶数', volume: true, gameid: 153},
-                {class: 'ds-icon-game-qqwfc sign new', id: '1-1-16', menuid: '132', title: '奇趣5分彩', volume: true, gameid: 165},
-                {class: 'ds-icon-game-qqsfc sign new', id: '1-1-15', menuid: '133', title: '奇趣10分彩', volume: true, gameid: 166}
+                {class: 'ds-icon-game-qqtxwfc sign new', id: '1-1-16', menuid: '132', title: '奇趣腾讯5分彩', volume: true, gameid: 165},
+                {class: 'ds-icon-game-qqtxsfc sign new', id: '1-1-15', menuid: '133', title: '奇趣腾讯10分彩', volume: true, gameid: 166}
               ]
             },
             {
