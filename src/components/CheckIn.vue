99724aac2f821e1178074cd8d0a21f08517e430e
brian <brian>
2019-12-03 18:49:38 +0800

    签到功能

---
+++ src/components/CheckIn.vue
@@ -0,0 +1,132 @@
+<template lang="jade">
+  .checkin-view
+    .checkin-content
+      .btn-close(@click="$emit('on-close')")
+      .inner-content.flex.flex-wp-wp
+        .activity-explain
+          .header.flex.flex-ai-c
+            span.ft24.color-green.ml20 每日
+            span.ft24 签到
+            span.ml15.icon-rect 
+            span.ml15 {{curMonth}}月您已经累计签到：
+            span.color-green {{checkinCount}}天
+          .rules
+            section.line
+              div.color-yellow.ft14 活动时间： 
+              p.ft12.mt05 2019-04-20 00:00:00——2031-04-30 23:59:59
+            section.line.mt20
+              div.color-yellow.ft14 活动内容： 
+              p.ft12.mt05 每天登入平台，投注达到1000元，即可参与消费签到，签到成功可以领取各种好礼，连续签到还有额外好礼相赠
+            section.mt20
+              div.color-yellow.ft14 领取时间：
+              p.ft12 3月您已经累计签到：2 天
+              p.ft12.mt05 每日签到立刻领取：2 元礼金
+        .checkin-calendar
+          CheckInCalendar(@on-choice="checkInHandler")
+        .line-more-prize
+        .checkin-days-explain.flex.flex-ai-c
+          .day-3
+          .day-5
+          .day-12
+          .day-20
+          .day-28
+        .contidion-tip
+          | 1.每天达到投注要求点击签到，当天日期及会显示已签到标志；
+          | 2.累计签到达到对应天数的点击领取即可获得对应奖励；
+          | 3体验金点击领取后会以现金券的形式发放至【优惠券】中，只需1倍流水即可提款
+</template>
+
+<script>
+import CheckInCalendar from './CheckInCalendar'
+export default {
+  name: 'checkin-view',
+  components: {
+    CheckInCalendar
+  },
+  data () {
+    return {
+      curMonth: 0,
+      checkinCount: 0
+    }
+  },
+  mounted() {
+    this.curMonth = new Date().getMonth() + 1
+  },
+  methods: {
+    checkInHandler() {
+
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+ .checkin-view
+  position fixed
+  width 100%
+  height 100%
+  z-index 9999
+  text-align left
+  $img-base-dir = "../assets/activity/checkin/"
+  bgImage($name)
+    background url($img-base-dir+$name+".png") center no-repeat
+  .btn-close
+    width 0.42rem
+    height 0.42rem
+    bgImage("icon-close");
+    cursor pointer
+    position absolute
+    right 1.80rem
+    top 0.50rem
+  .checkin-content
+    width 13.72rem
+    height 7.40rem
+    bgImage("dialog-bg");
+    margin 2.00rem auto 0
+    position relative
+  .inner-content
+    height 100%
+    padding .95rem 2.20rem 0.5rem 2.52rem
+    box-sizing border-box
+  .icon-rect
+    width 0.22rem
+    height 100%
+    bgImage("00");
+  .activity-explain
+    width 4.52rem
+    height 3.8rem
+    color #fff
+    .header
+      height 0.70rem
+  .checkin-calendar
+    width 4.44rem
+    height 3.8rem
+  .rules
+    padding 0.22rem 0.2rem 0 0.28rem
+    &>section 
+      padding-bottom 0.15rem
+    .line
+      border-bottom solid 1px #ffffff
+  .line-more-prize
+    width 100%
+    height 0.18rem
+    bgImage("line-more-prize");
+  .checkin-days-explain
+    width 100%
+    padding 0 0.3rem
+    justify-content space-between
+    box-sizing border-box
+    & > div
+      width 1.5rem
+      height 0.72rem
+  for n in 3 5 12 20 28
+    .day-{n}
+      bgImage("day-"+n);
+  .contidion-tip
+    padding 0.1rem 0.5rem 0.2rem 0.5rem
+    color #a595f0
+  .color-green
+    color #64bc1e 
+  .color-yellow
+    color #fedb1b
+</style>
---
+++ src/components/CheckInCalendar.vue
@@ -0,0 +1,91 @@
+<template lang="jade">
+  .checkin-calendar-view
+    .calendar-header.flex.flex-ai-c.ft14
+      .week(v-for="w in weeks") {{w}}
+    .calendar-days.flex.ft18
+      .day-item.flex.flex-ai-c.flex-jt-c(v-for="d in firstDay")
+        .day
+      .day-item.flex.flex-ai-c.flex-jt-c(v-for="d in daysOfMonth" @click="checkInHandler(d)" v-bind:class="[]")
+        .day {{d}}
+</template>
+
+<script>
+export default {
+  name: 'checkin-calendar-view',
+  data () {
+    return {
+      weeks: ['周日', '周一', '周二', '周三', '周四', '周五', '周六'],
+      daysOfMonth: 0,
+      firstDay: 0,
+      date: null
+    }
+  },
+  mounted() {
+    this.init()
+  },
+  methods: {
+    init() {
+      this.date = new Date();
+      this.firstDay = this.getFirstDayOfMonth(this.date);
+      this.daysOfMonth = this.getDaysOfMonth(this.date.getFullYear, this.date.getMonth);
+    },
+    checkInHandler(date) {
+      console.log('date=', date)
+      if (date !== this.date.getDate()) return;
+      // TODO checkin
+      console.log('do checkin')
+      this.$emit('on-choice', date)
+    },
+    getFirstDayOfMonth(date) {
+      const temp = new Date(date.getTime())
+      temp.setDate(1)
+      return temp.getDay()
+    },
+    getDaysOfMonth(year, month) {
+      if (month === 3 || month === 5 || month === 8 || month === 10) {
+        return 30
+      }
+      if (month === 1) {
+        return (year % 4 === 0 && year % 100 !== 0 || year % 400 === 0) ? 29 : 28
+      }
+      return 31
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+ .checkin-calendar-view
+  width 100%
+  $img-base-dir = "../assets/activity/checkin/"
+  bgImage($name)
+    background url($img-base-dir+$name+".png") center no-repeat
+    background-size 100%
+    color #fff
+  .calendar-header
+    height 0.60rem
+    color #202020
+    .week
+      width .62rem
+      text-align center
+      font-weight bold
+  .calendar-days
+    flex-wrap wrap
+    .day-item
+      width .62rem
+      margin 0.06rem 0 0.06rem 0
+      cursor pointer
+      &.checkin .day
+        bgImage("icon-circle-yellow")
+      &.checkined .day
+        bgImage("icon-circle-red")
+      &.uncheckin .day
+        bgImage("icon-circle-black")
+    .day 
+      width 0.4rem
+      height 0.4rem
+      line-height 0.4rem
+      text-align center
+      color #9b9b9b
+      background url("../assets/activity/checkin/icon-circle-gray.png") center no-repeat
+</style>
