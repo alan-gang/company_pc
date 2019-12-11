e8005162395a7a9f5f49621df55994227d083c17
brian <brian>
2019-12-11 16:47:49 +0800

    merge checkin

---
+++ src/components/StickyCheckIn.vue
@@ -0,0 +1,25 @@
+<template lang="jade">
+  .sticky-checkin-view(@click="checkInHandler")
+</template>
+
+<script>
+export default {
+  name: 'sticky-checkin',
+  methods: {
+    checkInHandler() {
+      this.$emit("on-open-checkin")
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+ .sticky-checkin-view
+  position fixed
+  right 0.80rem
+  bottom 0.60rem
+  width 1.10rem
+  height 1.15rem
+  background url("../assets/activity/checkin/daily-check-in.png") center no-repeat
+  cursor pointer
+</style>
